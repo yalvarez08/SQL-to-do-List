1f4cf9aa9a88f9481e2b146a626baef232bdc314
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-02-05 18:34:45 -0500

    updated pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -6,11 +6,29 @@ if (process.env.NODE_ENV === 'test') {
   databaseName = 'prime_testing'
 }
 
-const pool = new pg.Pool({
+let pool;
+
+if(process.env.DATABASE_URL) {
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  pool = new pg.Pool({
     host: 'localhost',
     port: 5432,
     database: databaseName,
-    allowExitOnIdle: true 
-})
+    allowExitOnIdle: true
+  });
+}
+
+// const pool = new pg.Pool({
+//     host: 'localhost',
+//     port: 5432,
+//     database: databaseName,
+//     allowExitOnIdle: true 
+// })
 
 module.exports = pool
